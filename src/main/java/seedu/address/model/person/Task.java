ef4480e453f291525e0c2c089499a59854b58adc
anir2212 <anir2212@gmail.com>
2026-03-16 12:48:27 +0800

    Add task and tasklist class

---
+++ src/main/java/seedu/address/model/person/Task.java
@@ -0,0 +1,29 @@
+package seedu.address.model.person;
+
+/**
+ * A class to represent a Task.
+ */
+public class Task {
+    private String taskName;
+    private String taskDescription;
+
+    /**
+     * Constructor for Task.
+     * @param taskName the name of the task.
+     * @param taskDescription the description of the task.
+     */
+    public Task(String taskName, String taskDescription) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+
+}
---
+++ src/main/java/seedu/address/storage/TaskListStorage.java
@@ -0,0 +1,24 @@
+package seedu.address.storage;
+
+import java.util.List;
+
+import seedu.address.model.person.Task;
+
+/**
+ * A class to represent the storage of TaskList.
+ */
+public class TaskListStorage {
+    private List<Task> tasks;
+
+    /**
+     * Constructor for TaskListStorage.
+     * @param tasks the list of tasks to be stored.
+     */
+    TaskListStorage(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+
+
+
+}
